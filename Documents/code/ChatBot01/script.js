d64f0b819cb21c85a6cc80e9a2d5df862bbade20
kian <156345342+ebrahimiG@users.noreply.github.com>
2025-10-25 10:02:56 +0330

    almost finishing the front

--- Documents/code/ChatBot01/script.js
+++ Documents/code/ChatBot01/script.js
@@ -11,8 +11,34 @@ function addMessage(message, classNme) {
     chatBox.scrollTop = chatBox.scrollHeight;
 }
 
+function showTyping(){
+    const typingDiv = document.createElement("div");
+    typingDiv.classList.add("message", "bot-message");
+    typingDiv.textContent = "AI is typing...";
+    chatBox.appendChild(typingDiv);
+    chatBox.scrollTop = chatBox.scrollHeight;
+    return typingDiv;
+}
+
+async function getBotReplay(userMessage){
+    
+}
+
 sendBtn.onclick = async () => {
     const message = userInput.value.trim();
     if (message === "") return;
     addMessage(message, "user-message");
+    userInput.value = ""
+
+    const typingDiv = showTyping();
+
+    const botReplay = await getBotReplay(message);
+    typingDiv.remove();
+    addMessage(botReplay,"bot-message");
+
+    localStorage.setItem("chatHistory", chatBox.innerHTML);
 }
+
+userInput.addEventListener("keypress",(e) => {
+    if (e.key === "Enter") sendBtn.click();
+})
