aa90ef0b2c920844d02bd6db6fd53a78880ee164
kian <156345342+ebrahimiG@users.noreply.github.com>
2025-11-18 17:05:26 +0330

    building more pages

--- DaraMovie4.5.2/DaraMovie/main/urls.py
+++ DaraMovie4.5.2/DaraMovie/main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.home_view,name='home')
+    path('',views.home_view,name='home'),
+    path('about/',views.about_view,name='about'),
 ]
--- DaraMovie4.5.2/DaraMovie/main/views.py
+++ DaraMovie4.5.2/DaraMovie/main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request):
     return render(request,'main/home.html')
+
+def about_view(request):
+    return render(request, 'main/about.html')
+    
