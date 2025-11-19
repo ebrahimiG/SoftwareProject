d7b77f99eadbaa1be03acece39d1164e0c5301d6
kian <156345342+ebrahimiG@users.noreply.github.com>
2025-11-19 20:15:44 +0330

    Create views.py

---
+++ Documents/code/moviemodel/django_project/movies/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Movie
+# Create your views here.
+
+
+def movie_view(request):
+    movies = Movie.objects.all()
+    context = {'movies':movies}
+    return render(request,'movies/movie_list.html',context)
