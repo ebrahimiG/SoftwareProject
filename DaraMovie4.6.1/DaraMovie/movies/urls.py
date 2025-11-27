1ce0bdc07180eb8c13350c804a0a3ca8520d1146
kian <156345342+ebrahimiG@users.noreply.github.com>
2025-11-27 18:58:07 +0330

    movie detail
    passing the info to movie_detail.html

--- DaraMovie4.6.1/DaraMovie/movies/urls.py
+++ DaraMovie4.6.1/DaraMovie/movies/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('all-movies/',views.all_movie_view,name='all_movies'),
+    path('movie-detail/<slug:slug>/',views.movie_detail_view,name='movie_detail'),
 ]
--- DaraMovie4.6.1/DaraMovie/movies/views.py
+++ DaraMovie4.6.1/DaraMovie/movies/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 # Create your views here.
 
+# a table for showing all movies
 def all_movie_view(request):
     movies = Movie.objects.all()
     context = {'movies':movies}
     return render(request,'movies/movie_all.html',context)
+
+# movie detail: 
+# getting the slug from <a href="{% url "movie_detial" movie.slug %}"> in home page. with this function, we get the info from database about that movie with same the slug and sending the info to movie detail page while rendering it.
+def movie_detail_view(request,slug):
+    movie = get_object_or_404(Movie,slug= slug)
+    context = {'movie':movie}
+    return render (request,'movies/movie_detail.html',context)
+
+
