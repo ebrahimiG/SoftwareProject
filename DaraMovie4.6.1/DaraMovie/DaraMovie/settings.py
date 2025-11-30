0794dec4d106cf843ee1569a9408a8d26c9eb6ec
kian <156345342+ebrahimiG@users.noreply.github.com>
2025-11-30 11:41:55 +0330

    working on genre detail page

--- DaraMovie4.6.1/DaraMovie/DaraMovie/settings.py
+++ DaraMovie4.6.1/DaraMovie/DaraMovie/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'DaraMovie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [BASE_DIR/'templates'], # for base.html
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
