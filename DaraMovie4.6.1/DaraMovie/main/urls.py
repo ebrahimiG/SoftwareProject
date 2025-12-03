e36103896bea0f8cb7e261eb424caee17c74988c
kian <156345342+ebrahimiG@users.noreply.github.com>
2025-12-03 16:45:23 +0330

    working on other pages

--- DaraMovie4.6.1/DaraMovie/main/urls.py
+++ DaraMovie4.6.1/DaraMovie/main/urls.py
@@ -3,4 +3,7 @@ from . import views
 urlpatterns = [
     path('',views.home_view,name='home'),
     path('about/',views.about_view,name='about'),
+    path('contact/',views.contact_view,name='contact'),
+    path('faq/',views.faq_view,name='faq'),
+    path('subscription/',views.subs_view,name='sub'),
 ]
--- DaraMovie4.6.1/DaraMovie/main/views.py
+++ DaraMovie4.6.1/DaraMovie/main/views.py
@@ -17,8 +17,21 @@ def home_view(request):
     return render(request,'main/home.html',context)
 
 
-
-
 # about us page
 def about_view(request):
     return render(request,'main/about.html')
+
+
+# contact us page
+def contact_view(request):
+    return render(request,'main/contact.html')
+
+
+# faq page
+def faq_view(request):
+    return render(request,'main/faq.html')
+
+
+#  page
+def subs_view(request):
+    return render(request,'main/subs.html')
