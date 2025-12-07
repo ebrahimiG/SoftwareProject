7cc06bd16fde86313f9e0e0c5fcc05632ec86d7d
kian <156345342+ebrahimiG@users.noreply.github.com>
2025-12-07 10:31:22 +0330

    register system practice

--- Documents/code/chatbot001/chatbot/chatbot_app/views.py
+++ Documents/code/chatbot001/chatbot/chatbot_app/views.py
@@ -3,8 +3,9 @@ from django.http import JsonResponse
 import ollama
 from django.contrib import auth
 from django.contrib.auth.models import User
-
-# Create your views here.
+from .models import Chat
+# to get the the user timezone
+from django.utils import timezone
 
 # if you get this error : ollama._types.ResponseError (status code: 503) , make sure to turn off the proxy
 
@@ -32,6 +33,9 @@ def ask_ollama(message):
 
 
 def chatbot_view(request):
+    # to show all chat history of the currnet user: and then pass the info to the return render as context
+    chats = Chat.objects.filter(user = request.user)
+    context = {'chats':chats}
     if request.method == "POST":
         # 1️⃣ storing the message that comes from js file:
         message = request.POST.get('Message')
@@ -39,10 +43,15 @@ def chatbot_view(request):
         # 2️⃣ send message and getting the response:
         response = ask_ollama(message)
 
+        # storing the chat histroy to the db: user = request.user means the user that currently loged in
+        # first message and response are the parameters from the Chat model
+        chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
+        chat.save()
+
         # 3️⃣ send response to js file:
         return JsonResponse({'response': response})
 
-    return render(request, 'chatbot_app/chatbot.html')
+    return render(request, 'chatbot_app/chatbot.html',context)
 
 
 def register_view(request):
@@ -80,15 +89,14 @@ def login_view(request):
         # checking if the user actually exists:
         user = auth.authenticate(request, username=username, password=password)
 
-        # if user exists: 
-        if user is not None: 
-            # log in the user; 
-            auth.login(request,user)
+        # if user exists:
+        if user is not None:
+            # log in the user;
+            auth.login(request, user)
             return redirect('chatbot')
         else:
             error_message = 'Invalid user!'
-            return render(request,'chatbot_app/login.html',{'error_message':error_message})
-
+            return render(request, 'chatbot_app/login.html', {'error_message': error_message})
 
     return render(request, 'chatbot_app/login.html')
 
