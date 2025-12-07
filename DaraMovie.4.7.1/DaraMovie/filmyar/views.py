ce0f96cf6ab14fbed16a46b60fe3898b0435ecb2
kian <156345342+ebrahimiG@users.noreply.github.com>
2025-12-07 11:25:38 +0330

    Update views.py

--- DaraMovie.4.7.1/DaraMovie/filmyar/views.py
+++ DaraMovie.4.7.1/DaraMovie/filmyar/views.py
@@ -5,17 +5,28 @@ import ollama
 # get message from js file
 # give it to ollama and get the response
 # send the response to js file to show
+
+# ask ollama
+def ask_ollama(message):
+    client = ollama.Client()
+    model = 'mistral' 
+    prompt = message
+
+    Response = client.generate(model=model, prompt=prompt)
+    response = Response.response
+
+    return response
+
+
+
+# filmyar and show chat and chat history
 def filmyar_view(request):
     # getting the message from script_filmyar.js
     if request.method =="POST":
         message = request.POST.get('message')
     
-        # ollama:
-        client = ollama.Client()
-        model = 'mistral'
-        prompt = message
-        Response = client.generate(model=model,prompt=prompt)
-        response = Response.response
+        # sending message to ollama and take the response:
+        response = ask_ollama(message)
 
         # giving the response to the script_filmyar.js
         return JsonResponse({'response':response})
