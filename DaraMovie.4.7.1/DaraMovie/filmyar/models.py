082000e23047c31eea329a65c63ef9cef0b2c1f3
kian <156345342+ebrahimiG@users.noreply.github.com>
2025-12-08 11:34:47 +0330

    Update models.py

--- DaraMovie.4.7.1/DaraMovie/filmyar/models.py
+++ DaraMovie.4.7.1/DaraMovie/filmyar/models.py
@@ -7,3 +7,9 @@ from django.contrib.auth.models import User
 
 class Chat(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
+    message = models.TextField()
+    response = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} : {self.user.message}'
