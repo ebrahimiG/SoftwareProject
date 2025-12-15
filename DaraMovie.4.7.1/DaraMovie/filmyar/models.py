fde61e4d799b93403a5a8928fb4a8aa4333b792f
kian <156345342+ebrahimiG@users.noreply.github.com>
2025-12-15 10:02:48 +0330

    Update models.py

--- DaraMovie.4.7.1/DaraMovie/filmyar/models.py
+++ DaraMovie.4.7.1/DaraMovie/filmyar/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 # creatin a Chat model to store and show user chat history:
 class Chat(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField()
     response = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
